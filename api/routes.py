f3c8421abc424a4f97531c6c7d8ad4f19725f7c0
bhunao <bh.felix@hotmail.com>
2023-09-03 18:54:32 -0300

    add: jinja2 base template

--- api/routes.py
+++ api/routes.py
@@ -1,42 +1,67 @@
-from fastapi import APIRouter
-from fastapi import Request
+import logging
+from typing import Annotated
+from fastapi import APIRouter, Form, Request
 from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
-from functions import get_note_by_name, list_files_in_directory
-from api.database import get_notes_desc, create_note_desc, all_notes_desc
+from functions import get_note_by_name
+from jinja2_fragments.fastapi import Jinja2Blocks
 
 
 router = APIRouter(prefix="/note", tags=["note"])
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Blocks(directory="templates")
+
+
+def hx(request: Request) -> bool:
+    return request.headers.get("HX-Request")
 
 
 @router.get("/", response_class=HTMLResponse)
-async def get_note(request: Request, name: str = None, edit: bool = False):
-    if not name:
-        n = all_notes_desc()
-        return templates.TemplateResponse(
-            "index.html", {
-                "request": request,
-                "notes_list": list_files_in_directory("../../notes/z/"),
-            })
-    if edit:
-        return templates.TemplateResponse(
-            "edit_note.html", {
-                "request": request,
-                "note_name": name,
-                "note_content": get_note_by_name(name)
-            })
-    create_note_desc(name)
+async def home(request: Request):
+    logging.info("home")
+    print("HOME")
+    return templates.TemplateResponse(
+        "index.html", {
+            "request": request,
+        },
+        block_name="content" if hx(request) else None,
+    )
+
+
+@router.get("/edit/", response_class=HTMLResponse)
+async def edit_note(request: Request, name: str):
+    print(*request.headers.items(), "\n")
+    logging.info("EDIT NOTE")
+    print("EDIT_note", name, hx(request))
+    return templates.TemplateResponse(
+        "edit_note.html", {
+            "request": request,
+            "note_name": name,
+            "note_content": get_note_by_name(name)
+        },
+        block_name="content" if hx(request) else None,
+    )
+
+
+@router.get("/{name}", response_class=HTMLResponse)
+async def get_note(request: Request, name: str):
+    return templates.TemplateResponse(
+        "edit_note.html", {
+            "request": request,
+            "note_name": name,
+            "note_content": get_note_by_name(name)
+        },
+        block_name="content" if hx(request) else None,
+    )
+
+
+@router.post("/search/", response_class=HTMLResponse)
+async def search_note(request: Request, name: Annotated[str, Form()]):
+    print(name, hx(request))
+    # TODO -> search algorithm
     return templates.TemplateResponse(
         "note.html", {
             "request": request,
             "note_name": name,
             "note_content": get_note_by_name(name)
-        })
-
-
-@router.post("/")
-async def save_note(name: str, content: str):
-    print(name)
-    print(content)
-    return name, content
+        },
+        block_name="content" if hx(request) else None,
+    )
