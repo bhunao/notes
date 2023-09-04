1daa31b434a6caba9d1c8598362a1d03bafcd649
bhunao <bh.felix@hotmail.com>
2023-09-03 22:58:23 -0300

    add: index page notes

--- api/routes.py
+++ api/routes.py
@@ -2,7 +2,7 @@ import logging
 from typing import Annotated
 from fastapi import APIRouter, Form, Request
 from fastapi.responses import HTMLResponse
-from api.functions import get_note_by_name
+from api.functions import get_note_by_name, list_files_in_directory, notes_view
 from api.markdown import html, text_index
 from jinja2_fragments.fastapi import Jinja2Blocks
 
@@ -16,12 +16,28 @@ def hx(request: Request) -> bool:
 
 
 @router.get("/", response_class=HTMLResponse)
-async def home(request: Request):
+async def home(request: Request, name: str = None):
     logging.info("home")
-    print("HOME")
+    print("HOME", name)
+    if name:
+        content = get_note_by_name(name)
+        index = text_index(content)
+        content = html(content)
+        return templates.TemplateResponse(
+            "note.html", {
+                "request": request,
+                "note_name": name,
+                "index": index,
+                "note_content": content,
+            },
+            block_name="content" if hx(request) else None,
+        )
+    print("awsçldkfjhawdçlkfghj")
     return templates.TemplateResponse(
         "index.html", {
             "request": request,
+            "notes_list": list_files_in_directory("../../notes/z/"),
+            "notes_view": notes_view(),
         },
         block_name="content" if hx(request) else None,
     )
@@ -37,18 +53,6 @@ async def edit_note(request: Request, name: str):
         "note_name": name,
         "note_content": get_note_by_name(name)
     },
-        block_name="content" if hx(request) else None,
-    )
-
-
-@router.get("/{name}", response_class=HTMLResponse)
-async def get_note(request: Request, name: str):
-    return templates.TemplateResponse(
-        "edit_note.html", {
-            "request": request,
-            "note_name": name,
-            "note_content": get_note_by_name(name)
-        },
         block_name="content" if hx(request) else None,
     )
 
