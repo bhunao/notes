f92620e9f2d66268f684720a2b1cb031f8cc6ca3
bhunao <bh.felix@hotmail.com>
2023-09-26 18:06:00 -0300

    fix: button place and css

--- src/components/note.nim
+++ src/components/note.nim
@@ -5,9 +5,9 @@ import db_connector/db_sqlite
 proc htmlNote*(row: Row, content: string): string =
   var res = buildHtml(tdiv):
     tdiv(class="container p-3"):
-      verbatim(content)
-
-      button(hx-get="/edit/"&row[0], hx-trigger="click", hx-target="#content", hx-swap="outerHTML", hx-push-url="true"):
+      button(class="btn btn-warning btn-sm", hx-get="/edit/"&row[0], hx-trigger="click", hx-target="#content", hx-swap="outerHTML", hx-push-url="true"):
         text "EDIT"
+      hr(class="p-1")
+      verbatim(content)
 
   result = $res
