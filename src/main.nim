1364b78463733c5c4815ce508a4fad41e551daea
bhunao <bh.felix@hotmail.com>
2023-09-26 17:51:28 -0300

    add: new endpoints and templates

--- src/main.nim
+++ src/main.nim
@@ -6,10 +6,47 @@ import std/strutils
 import markdown
 import components/html_template
 import components/note
+import components/search
+import components/edit_note
 
 serve("127.0.0.1", 5000):
+  get "/edit/{id:int}":
+    echo req
+    var 
+      row = getRowById(id)
+      fullPath = row[2] / row[1].addFileExt "md"
+      config = markdown.initGfmConfig()
+      html = openNote(fullPath)
+      editNote = $htmlEditNote(row, html)
+
+      
+    if req.headers.hasKey("hx-request"):
+      answerHtml(req, editNote, Http200)
+    else:
+      answerHtml(req, $htmlTemplate(editNote), Http200)
+
   get "/":
     answerHtml(req, $htmlTemplate(), Http200)
+  post "/search/":
+    echo "body: ", happyx.parseXWwwFormUrlencoded(req.body)
+    var 
+      name = happyx.parseXWwwFormUrlencoded(req.body)["name"]
+      isHxRequest = req.headers.hasKey("hx-request")
+      result = getRow(name, "")
+    
+    if result.len == 1:
+      var 
+        row = result[0]
+        fullPath = row[2] / row[1].addFileExt "md"
+        config = markdown.initGfmConfig()
+        html = markdown(openNote(fullPath), config)
+      if isHxRequest:
+        answerHtml(req, $htmlNote(row, html), Http200)
+      else:
+        answerHtml(req, $htmlTemplate(html), Http200)
+    else:
+      answerHtml(req, $htmlSearch(result), Http200)
+
   get "/note/{name}":
     var result = getRow(name, "")
     echo result
@@ -20,7 +57,7 @@ serve("127.0.0.1", 5000):
         row = result[0]
         fullPath = row[2] / row[1].addFileExt "md"
         config = markdown.initGfmConfig()
-        html = $testComponent markdown(openNote(fullPath), config)
+        html = $htmlNote(row, markdown(openNote(fullPath), config))
       answerHtml(req, html, Http200)
 
     else:
