9c2c3401f946107bc2482ff8a68c584ba751fdf2
bhunao <bh.felix@hotmail.com>
2023-09-30 10:59:30 -0300

    add: error reponse when invalid id

--- src/views.nim
+++ src/views.nim
@@ -1,5 +1,9 @@
+import std/os
 import prologue
 import ./components/[note, edit_note, search, html_template]
+import db_connector/db_sqlite
+import ./utils
+import markdown
 
 
 proc getIndex*(ctx: Context) {.async.} =
@@ -10,7 +14,20 @@ proc getNote*(ctx: Context) {.async.} =
   # TODO: return note content inside a htmlNote
   echo "============ getNote ============"
   echo ctx.getPathParams("id")
-  resp htmlTemplate()
+  var id = ctx.getPathParams("id")
+
+  let db = open("mytest.db", "", "", "")
+  var row = db.getRow( sql"SELECT * FROM notes WHERE id = ?", id)
+  db.close()
+
+  if row[0] == "":
+    resp "invalid id", Http404
+  else:
+    var 
+      note = openNote(row[2] / row[1].addFileExt ".md" )
+      config = markdown.initGfmConfig()
+      html = markdown(note, config)
+    resp htmlTemplate html
 
 proc postNote*(ctx: Context) {.async.} =
   # TODO: save the content to the file and update in database
@@ -25,6 +42,7 @@ proc postSearch*(ctx: Context) {.async.} =
   # TODO: get search results and return them
   echo ctx.request
   echo ctx.getFormParams("name")
+  var name = ctx.getFormParams("name")
   resp htmlSearch()
 
 proc getEdit*(ctx: Context) {.async.} =
