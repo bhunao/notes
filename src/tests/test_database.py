495565c9f136b32c764d3dd14330e88b7a489424
bhunao <bh.felix@hotmail.com>
2023-10-15 00:03:27 -0300

    tests dont work

--- src/tests/test_database.py
+++ src/tests/test_database.py
@@ -2,18 +2,19 @@ from .. import models
 from sqlmodel import SQLModel, create_engine, Session, select
 
 
+Note = models.Note
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 engine = create_engine(sqlite_url, echo=True)
 SQLModel.metadata.create_all(engine)
-valid_note = models.Note(name="test_file01", path="/teste_folder")
+valid_note = Note(name="test_file01", path="/teste_folder")
 
 
 class TestDatabase:
     def setup_class(self):
         self.engine = engine
-        self.valid_note = models.Note(name="test_file01", path="/teste_folder")
+        self.valid_note = Note(name="test_file01", path="/teste_folder")
 
     def teardown(self):
         with Session(self.engine) as session:
@@ -25,11 +26,11 @@ class TestDatabase:
             session.add(self.valid_note)
             session.commit()
 
-            statement = select(models.Note).where(
-                models.Note.name == self.valid_note.name,
-                models.Note.path == self.valid_note.path,
-                models.Note.type == self.valid_note.type,
-                models.Note.parent == self.valid_note.parent,
+            statement = select(Note).where(
+                Note.name == self.valid_note.name,
+                Note.path == self.valid_note.path,
+                Note.type == self.valid_note.type,
+                Note.parent == self.valid_note.parent,
             )
             result = session.exec(statement).first()
             assert result is not None
@@ -41,11 +42,11 @@ def test_valid_note():
         session.add(valid_note)
         session.commit()
 
-        statement = select(models.Note).where(
-            models.Note.name == valid_note.name,
-            models.Note.path == valid_note.path,
-            models.Note.type == valid_note.type,
-            models.Note.parent == valid_note.parent,
+        statement = select(Note).where(
+            Note.name == valid_note.name,
+            Note.path == valid_note.path,
+            Note.type == valid_note.type,
+            Note.parent == valid_note.parent,
         )
         result = session.exec(statement).first()
         assert result is not None
